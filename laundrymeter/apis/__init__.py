0dbc6f8329b5e57e1179ff8331d99acbb43e2d55
Max Stabel <M4a1x@users.noreply.github.com>
2018-09-14 12:34:09 +0200

    Enable and document auth for the whole api

--- laundrymeter/apis/__init__.py
+++ laundrymeter/apis/__init__.py
@@ -1,23 +1,36 @@
 from flask import Blueprint
 from flask_restplus import Api
 
+from .machine import api as machine
 from .email import api as email
 from .telegram import api as telegram
-from .auth import api as auth
+from .auth import api as auth, auth as basic_auth
 
 # Create Flask Blueprint
 bp = Blueprint('api', __name__, url_prefix='/api')
 
+# Document HTTP Basic Auth
+authorizations = {
+    'basicAuth': {
+        'type': 'http',
+    }
+}
+
+# TODO: Test if default 'decorators' work as expected
 # Use the Blueprint for the main api
 api = Api(bp,
     title='Washing Machine Service Api',
     version='0.1.0',
     description='Provides various endpoints and interfaces for getting the status of the Washing machine.',
+    authorizations=authorizations,
+    security='basicAuth' # Document authorization for all endpoints
+    decorators=basic_auth.login_required # Require authorizations for all endpoints
     # All API metadatas
     # TODO: Input metadata defining the api
 )
 
 # Add api namespaces to the main api
+api.add_namespace(machine, path='/machine')
 api.add_namespace(auth, path='/auth')
 api.add_namespace(email, path='/email')
 api.add_namespace(telegram, path='/telegram')
