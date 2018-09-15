7515fda312dbf8a65d91b81e9bdfd2bf410d8269
Max Stabel <M4a1x@users.noreply.github.com>
2018-09-15 11:49:55 +0200

    Added try/except for plug connection

--- laundrymeter/wm_poller.py
+++ laundrymeter/wm_poller.py
@@ -33,23 +33,35 @@ def notify_all():
 
 
 def update_washing_mashine():
-    emeter = plug.get_emeter_realtime()
     now = datetime.utcnow()
-    running = emeter['power_mw']/1000 > 10 # Drawing more than 10W means the machine is running... TODO: get actual values!
-    last = WashingMachine.query.order_by(desc('timestamp')).first()
-    if last and last.running == running:
-        last_changed = last.last_changed
-    else:
-        last_changed = now
 
-    washing_machine = WashingMachine(timestamp=now,
-                                     running=running,
-                                     last_changed=last_changed,
-                                     voltage=emeter['voltage_mv']/1000,
-                                     current=emeter['current_ma']/1000,
-                                     power=emeter['power_mw']/1000,
-                                     total_power=emeter['total_wh']/1000)
-    
+    # Try to get a reading. If it fails, enter default values into the database.
+    try:
+        # TODO: Log when no connection.
+        emeter = plug.get_emeter_realtime()
+        running = emeter['power_mw']/1000 > 10 # Drawing more than 10W means the machine is running... TODO: get actual value!
+        last = WashingMachine.query.order_by(desc('timestamp')).first()
+        if last and last.running == running:
+            last_changed = last.last_changed
+        else:
+            last_changed = now
+
+        washing_machine = WashingMachine(timestamp=now,
+                                        running=running,
+                                        last_changed=last_changed,
+                                        voltage=emeter['voltage_mv']/1000,
+                                        current=emeter['current_ma']/1000,
+                                        power=emeter['power_mw']/1000,
+                                        total_power=emeter['total_wh']/1000)
+    except:
+        washing_machine = WashingMachine(timestamp=now,
+                                        running=False,
+                                        last_changed=None,
+                                        voltage=0,
+                                        current=0,
+                                        power=0,
+                                        total_power=0)
+
     db.session.add(washing_machine)
     db.session.flush()
 
@@ -59,7 +71,7 @@ def update_washing_mashine():
         washing_machine = WashingMachine.query.order_by(asc('timestamp')).first()
         db.session.delete(washing_machine)
     
-    if count > 6400000:
+    if count > 6400000: # Just to be sure..?
         db.session.query(WashingMachine).delete()
 
     db.session.commit()
