f9d4b085f91bc6b180a9d7ae88ac0c44408c6b45
Max Stabel <M4a1x@users.noreply.github.com>
2018-09-16 17:49:43 +0200

    Improved auth documentation

--- laundrymeter/apis/__init__.py
+++ laundrymeter/apis/__init__.py
@@ -12,9 +12,7 @@ bp = Blueprint('api', __name__)
 # Document HTTP Basic Auth
 authorizations = {
     'basicAuth': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'       
+        'type': 'basic'      
     }
 }
 
@@ -26,13 +24,12 @@ api = Api(bp,
     description='Provides various endpoints and interfaces for getting the status of the Washing machine.',
     authorizations=authorizations,
     security='basicAuth', # Document authorization for all endpoints
-    decorators=[basic_auth.login_required] # Require authorizations for all endpoints
     # All API metadatas
     # TODO: Input metadata defining the api
 )
-Api()
 
 # Add api namespaces to the main api
+api.namespaces.clear() # TODO: Remove this after upgrading to flask-restplus 0.12.0
 api.add_namespace(machine, path='/machine')
 api.add_namespace(auth, path='/auth')
 api.add_namespace(email, path='/email')
