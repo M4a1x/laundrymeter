7c1e4c0c8439afcca4751e9728a11d6633b3540b
Max Stabel <M4a1x@users.noreply.github.com>
2018-10-13 15:11:15 +0200

    Capture flask log in gunicorn logfile.

--- start_production.sh
+++ start_production.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pipenv run gunicorn -w 1 -b 0.0.0.0:8000 --error-logfile "laundrymeter-err.log" --log-file "laundrymeter.log" --log-level debug "laundrymeter:create_app()"
+pipenv run gunicorn -w 1 -b 0.0.0.0:8000 --capture-output --error-logfile "laundrymeter-err.log" --log-file "laundrymeter.log" --log-level debug "laundrymeter:create_app()"
